624783a440e1e7ebfd3c5b28a95080626cb51cf7
nectordev <p.vlad-rus@mail.ru>
2024-02-19 01:00:58 +0300

    How to use trailing closures and shorthand syntax

--- day_9_Closures/main.playground/Contents.swift
+++ day_9_Closures/main.playground/Contents.swift
@@ -10,6 +10,11 @@ var greetCopy = greetUser
 greetCopy()
 
 
+var test = [1, 2, 3, 4]
+
+test.remove(at: test.firstIndex(of: 3)!)
+print(test)
+
 let sayHello = {
     print("Say hello!")
 }
@@ -36,3 +41,48 @@ let payment = { (user: String) -> Bool in
     print("Paying \(user)…")
     return true
 }
+
+
+let team = ["Gloria", "Suzanne", "Piper", "Tiffany", "Tasha"]
+
+let captainFirstTeam = team.sorted(by: { (name1: String, name2: String) -> Bool in
+    if name1 == "Suzanne" {
+        return true
+    } else if name2 == "Suzanne" {
+        return false
+    }
+
+    return name1 < name2
+})
+
+print(captainFirstTeam)
+
+let captainFirstTeam1 = team.sorted { name1, name2 in
+    if name1 == "Suzanne" {
+        return true
+    } else if name2 == "Suzanne" {
+        return false
+    }
+
+    return name1 < name2
+}
+
+let captainFirstTeam2 = team.sorted {
+    if $0 == "Suzanne" {
+        return true
+    } else if $1 == "Suzanne" {
+        return false
+    }
+
+    return $0 < $1
+}
+
+let reverseTeam = team.sorted {
+    return $0 > $1
+}
+
+let tOnly = team.filter { $0.hasPrefix("T") }
+print(tOnly)
+
+let uppercaseTeam = team.map { $0.uppercased() }
+print(uppercaseTeam)
